cd31b2b1a06ad95b66d35dd7c79f4c09e3f78caa
RabinThapa1998 <imrabin1998@gmail.com>
2022-10-06 18:58:37 +0545

    ✨ Feat: Update Contestant Delete with Program

--- src/controllers/v1/dashboard/programs/remove.ts
+++ src/controllers/v1/dashboard/programs/remove.ts
@@ -1,19 +1,31 @@
 import { Request, Response } from "express";
 import { BadRequestError } from "../../../../common/errors/bad-request-error";
 import { Program } from "../../../../models/programs";
+import { Contestant } from "../../../../models/contestants";
 
 const deleteProgram = async (req: Request, res: Response) => {
   try {
-    const { id } = req.body;
+    const { id } = req.params;
+    console.log("🚀 ~ file: remove.ts ~ line 9 ~ deleteProgram ~ id", id);
 
-    const program = await Program.deleteOne({ _id: id });
-    if (!program) {
+    const _program = await Program.findById(id);
+
+    if (!_program) {
       throw new BadRequestError("Program not found");
     }
+    const _contestant = await Contestant.deleteMany({ program: id });
+    console.log(
+      "🚀 ~ file: remove.ts ~ line 16 ~ deleteProgram ~ _contestant",
+      _contestant
+    );
 
-    return res
-      .status(200)
-      .json({ message: "program deleted successfully", data: program });
+    const program = await Program.deleteOne({ _id: id });
+
+    return res.status(200).json({
+      message: "program deleted successfully",
+      data: program,
+      contestant: _contestant.deletedCount,
+    });
   } catch (error) {
     throw new BadRequestError(
       (error as any).message
